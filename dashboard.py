a0e6faac9de9f80652626fa7d60675bcb4096adc
git-galluppakistan <galluppakistan2@gmail.com>
2025-12-22 17:47:50 +0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -132,7 +132,7 @@ if df is not None:
     ignore = [prov_col, reg_col, sex_col, dist_col, tehsil_col, edu_col, age_col, "Mouza", "Locality", "PCode", "EBCode"]
     questions = [c for c in df.columns if c not in ignore]
     
-    default_target = "Marital status (S4C7)"
+    default_target = "Marital Status (S4C7)"
     default_index = questions.index(default_target) if default_target in questions else 0
     target_q = st.selectbox("Select Question to Analyze:", questions, index=default_index)
 
@@ -328,3 +328,4 @@ if df is not None:
 
 else:
     st.info("Awaiting Data...")
+
